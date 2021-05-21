a3a727d3906f0e5e03188664819d5f77e74c07e0
sergey <skalutsky@gmail.com>
2021-05-21 09:11:26 +0300

    hot reload finally works with forge

---
+++ test/src/App.tsx
@@ -0,0 +1,31 @@
+import { hot } from 'react-hot-loader';
+import { useState } from 'react'
+import Console from './components/Console'
+import LeftSideBar from './components/LeftMenu'
+import WorkspaceBar from './components/WorkspaceBar'
+import './App.css'
+
+
+const Main = () => {
+  const [output, setOutput] = useState('');
+  return (
+    <>
+      <LeftSideBar />
+      <div className='right-side-bar'>
+        <div className='workspace'>
+          <WorkspaceBar output={output} setOutput={setOutput}/>
+        </div>
+        <Console output={output} />
+      </div>
+    </>
+  )
+}
+
+function App() {
+  return (
+    <Main />
+  );
+}
+
+
+export default hot(module)(App);
