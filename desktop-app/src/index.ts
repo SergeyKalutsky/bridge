1d35e2eeb21f0b4bb368ab641b086ba5ca18acee
sergey <skalutsky@gmail.com>
2021-05-22 17:53:55 +0300

    nodegit works(needed external libraries)

--- desktop-app/src/index.ts
+++ desktop-app/src/index.ts
@@ -6,6 +6,7 @@ import {Git} from "nodegit"
 declare const MAIN_WINDOW_WEBPACK_ENTRY: any;
 
 
+
 // Handle creating/removing shortcuts on Windows when installing/uninstalling.
 if (require('electron-squirrel-startup')) { // eslint-disable-line global-require
   app.quit();
