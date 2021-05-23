8880d2e95fbd5ce3a61c566621e5abc18a504637
sergey <skalutsky@gmail.com>
2021-05-23 21:34:29 +0300

    fixed bug with a listener

--- desktop-app/src/components/Console.tsx
+++ desktop-app/src/components/Console.tsx
@@ -1,33 +1,30 @@
+import { useState } from 'react';
 import { useRef, useEffect } from 'react'
 import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
 import { ipcRenderer } from "electron";
 import '../App.css'
 
-type propItems = {
-  // output: JSX.Element[]
-  output: string
-}
+
 
 const Console = () => {
-  let output: JSX.Element[] = []
-  const messagesEndRef = useRef(null)
+  // const messagesEndRef = useRef(null)
 
-  ipcRenderer.on('stdout', async (event, arg) => {
-    output = [...output, <p key={output.length}>{'>>> '+arg}</p>]
-    console.log(output)
-  })
-  const scrollToBottom = () => {
-    messagesEndRef.current?.scrollIntoView({ behavior: "smooth" })
+  const [output, setOutput] = useState([]);
+  const onData = (event, data: string) => {
+    setOutput([...output, <p key={output.length}>{'>>> '+data}</p>])
+    ipcRenderer.removeListener('stdout', onData)
   }
-  useEffect(() => {
-    scrollToBottom()
-  }, [output]);
+  // on init
+  ipcRenderer.on('stdout', onData)
+  // on deconstruct
+  // ipcRenderer.removeListener('stdout', onData)
 
   return (
     <div className='console'>
       <KeyboardArrowDownIcon />
       <div className='console-output'>
-      {output}
+        {output}
+        
       </div>
     </div>
   );
