096242e2ec3132ee311332043244b998929ea833
Kalutsky Sergey <SKalutsky@optimum.businesscar.ru>
2021-05-25 16:26:22 +0300

    split componens on several folders

--- desktop-app/src/components/git/Workspace.tsx
+++ desktop-app/src/components/git/Workspace.tsx
@@ -2,11 +2,6 @@ import ReactDiffViewer from 'react-diff-viewer';
 import WorkspaceTab from './WorkspaceTab'
 import Console from './Console'
 
-
-
-// https://github.com/praneshr/react-diff-viewer#readme unified verision
-// use for diff viewing
-
 const oldCode = `
 const a = 10
 const b = 10
@@ -28,10 +23,6 @@ if(a === 10) {
 console.log('hello world')
 `;
 
-// https://commonmark.org/help/
-// this is for task editing
-// https://github.com/remarkjs/react-markdown
-
 const WorkspaceGit = (): JSX.Element => {
   return (
     <div className='workspace'>
@@ -49,7 +40,7 @@ const WorkspaceGit = (): JSX.Element => {
 const Workspace = (): JSX.Element => {
   return (
     <WorkspaceGit />
-    )
+  )
 }
 
 export default Workspace
