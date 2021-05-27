3b325b259749e0794340c9f89f27476b950447ea
sergey <skalutsky@gmail.com>
2021-05-27 20:31:44 +0300

    made bare repo

--- api/main.py
+++ api/main.py
@@ -1,11 +1,13 @@
-from fastapi import FastAPI
+import os
 from git import Repo
+from fastapi import FastAPI
 
 app = FastAPI()
 
-
 @app.get("/")
 async def root():
+    bare_repo = Repo.init('bare-repo', bare=True)
+    assert bare_repo.bare
     return {"message": "Hello World!"}
 
 @app.get("/items/{item_id}")
