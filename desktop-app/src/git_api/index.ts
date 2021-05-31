a72c80f3738f0742b2ccac62f0ac0b0046a5d9c8
Kalutsky Sergey <SKalutsky@optimum.businesscar.ru>
2021-05-31 17:13:15 +0300

    init works with homedir

--- desktop-app/src/git_api/index.ts
+++ desktop-app/src/git_api/index.ts
@@ -1,14 +1,23 @@
-import {promises as fs} from 'fs'
+import { promises as fs } from 'fs'
+import path from 'path'
+import os from 'os'
 import simpleGit, { SimpleGit } from 'simple-git';
 
-
-
 const init = (remote: string, branch: string): void => {
-  const git: SimpleGit = simpleGit();
-  git.clone(remote)
+  let git: SimpleGit = simpleGit();
+  const repo = remote.split('/').pop().split('.')[0]
+  const repo_path = path.join(os.homedir(), repo)
+  git.clone(remote, repo_path)
+  git = simpleGit(repo_path, { binary: 'git' })
   git.branch([branch])
-  const repo_dir = remote.split('/').pop().split('.')[0]
-  fs.mkdir(`./${repo_dir}/${branch}`, { recursive: true });
+  fs.mkdir(path.join(repo_path, branch));
 }
 
+// const git: SimpleGit = simpleGit('fluffy-meme', { binary: 'git' });
+// console.log(git.pull())
+// const pull = (branches: string[])=> {
+
+
+// }
+// console.log(os.homedir())
 init('https://github.com/SergeyKalutsky/fluffy-meme.git', 'teacher')
