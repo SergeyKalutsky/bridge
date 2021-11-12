b8642fa9b6f44544e5190d66cbe661a4d8476a72
Kalutsky Sergey <SKalutsky@optimum.businesscar.ru>
2021-11-12 16:07:55 +0300

    gitlab and api works

---
+++ api/gitlabapi.py
@@ -0,0 +1,34 @@
+import gitlab
+
+gl = gitlab.Gitlab('https://gitlab.bridgeacross.xyz',
+                   private_token='Yyw3diYYbsAimWmXpwn2')
+
+
+def create_project(user_name, project_name):
+    user = gl.users.list(username=user_name)[0]
+    user.projects.create(
+        {'name': project_name, 'description': 'something_here'})
+
+
+def get_user_projects(user_name):
+    user = gl.users.list(username=user_name)[0]
+    return user.projects.list()
+
+
+def delete_user_project(user_name, project_name):
+    projects = get_user_projects(user_name)
+    for project in projects:
+        if project.name == project_name:
+            gl.projects.delete(project.id)
+
+
+def create_user(email, password, username, name):
+    gl.users.create({'email': email,
+                     'password': password,
+                     'username': username,
+                     'name': name})
+
+
+def delete_user(username):
+    user = gl.users.list(search=username)[0]
+    gl.users.delete(user.id)
