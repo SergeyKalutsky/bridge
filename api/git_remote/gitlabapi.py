8bb30392eacf17025025439a36ffd08dfa57fff4
SergeyKalutsky <skalutsky@gmail.com>
2021-11-16 23:16:57 +0300

    added memer function

--- api/git_remote/gitlabapi.py
+++ api/git_remote/gitlabapi.py
@@ -1,3 +1,5 @@
+# sergey ID 34
+# Jdoe ID 35
 import gitlab
 
 gl = gitlab.Gitlab('https://gitlab.bridgeacross.xyz',
@@ -6,9 +8,8 @@ gl = gitlab.Gitlab('https://gitlab.bridgeacross.xyz',
 
 def create_project(user_name, project_name, description):
     user = gl.users.list(username=user_name)[0]
-    user.projects.create({'name': project_name, 
+    user.projects.create({'name': project_name,
                           'description': description})
-    
 
 
 def get_user_projects(user_name):
@@ -33,3 +34,14 @@ def create_user(email, password, username, name):
 def delete_user(username):
     user = gl.users.list(search=username)[0]
     gl.users.delete(user.id)
+
+
+def get_project_by_name(name):
+    return gl.projects.list(search=name)[0]
+
+
+def add_project_member(user_id, project_id):
+    project = gl.projects.get(project_id)
+    project.members.create({'user_id': user_id,
+                            'access_level': gitlab.DEVELOPER_ACCESS})
+
