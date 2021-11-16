c26b56bd7f9b8a3e56985338d96a8cbdcd7c1554
Kalutsky Sergey <SKalutsky@optimum.businesscar.ru>
2021-11-16 17:35:43 +0300

    added more logic

--- desktop-app/src/components/projects/ProjectsMenu.tsx
+++ desktop-app/src/components/projects/ProjectsMenu.tsx
@@ -1,3 +1,6 @@
+// Add icons on focus
+// Pencil to edit, and checbox to select
+
 import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline';
 import IconButton from '@material-ui/core/IconButton';
 import { makeStyles } from '@material-ui/core/styles';
