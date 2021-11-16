2896cb9fc6e3a5999e06296715b1de5d45eda504
Kalutsky Sergey <SKalutsky@optimum.businesscar.ru>
2021-11-16 17:20:56 +0300

    creating project form done but badly

--- desktop-app/src/components/projects/ProjectsCreate.tsx
+++ desktop-app/src/components/projects/ProjectsCreate.tsx
@@ -1,14 +1,31 @@
-// for creating we need
-// 1. Project name
-// 2. Description
-// 3. Status(Public/Private)
-
+import '../../assets/css/ProjectsCreate.css'
 
 
 const ProjectsCreate = (): JSX.Element => {
     return (
-        <div>
-            CREATE
+        <div className='menu'>
+            <div className='crete-inputs row'>
+                <div className='row'>
+                    <input type="text" placeholder='Название проекта' />
+                </div>
+                <div className='row'>
+                    <textarea  placeholder='Описание' />
+                </div>
+                <div className='row'>
+                    <div>
+                        <input type="radio" id="huey" name="drone" value="huey"
+                            checked />
+                        <label>Публичный</label>
+                    </div>
+                    <div>
+                        <input type="radio" id="dewey" name="drone" value="dewey" />
+                        <label>Частный</label>
+                    </div>
+                </div>
+                <div className='row'>
+                    <button>Создать проект</button>
+                </div>
+            </div>
         </div>
     )
 
