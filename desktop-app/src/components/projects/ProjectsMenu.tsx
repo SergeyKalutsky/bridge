7721d330abd403432aa9c914a068bd79719fd919
Kalutsky Sergey <SKalutsky@optimum.businesscar.ru>
2021-11-17 15:37:05 +0300

    resized icons

--- desktop-app/src/components/projects/ProjectsMenu.tsx
+++ desktop-app/src/components/projects/ProjectsMenu.tsx
@@ -1,6 +1,7 @@
 import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTrashAlt } from '@fortawesome/free-solid-svg-icons'
+import { faSearch } from '@fortawesome/free-solid-svg-icons'
 import { useState } from 'react'
 import IconButton from '@material-ui/core/IconButton';
 import { makeStyles } from '@material-ui/core/styles';
@@ -9,12 +10,20 @@ import '../../assets/css/ProjectsMenu.css'
 const useStyles = makeStyles((theme) => ({
   menuIcon: {
     '& svg': {
-      fontSize: 45
+      fontSize: 30
     },
-    'margin-left': '50px',
+    'margin-left': '30px',
     'color': '#b3afb0',
     'justify-content': 'flex-end'
 
+  },
+  menuIconSerach: {
+    '& svg': {
+      fontSize: 25
+    },
+    // 'margin-left': '5px',
+    'color': '#b3afb0',
+    'justify-content': 'flex-end'
   }
 })
 );
@@ -41,8 +50,8 @@ const ProjectSelect = ({ project }: ProjectProp): JSX.Element => {
     <div className='project'
       onMouseOver={() => { setActive(true) }}
       onMouseLeave={() => { setActive(false) }}>
-      <span className={active == true ? ' selected': ''}>{project}</span>
-      {active && <TrashButton />} 
+      <span className={active == true ? ' selected' : ''}>{project}</span>
+      {active && <TrashButton />}
     </div>
   )
 }
@@ -58,6 +67,10 @@ const ProjectsMenu = ({ setIsCreate }: Setter): JSX.Element => {
         <IconButton className={classes.menuIcon}
           onClick={() => { setIsCreate(true) }}>
           <AddCircleOutlineIcon />
+        </IconButton >
+        <IconButton className={classes.menuIconSerach}
+          onClick={() => { setIsCreate(false) }}>
+          <FontAwesomeIcon icon={faSearch} />
         </IconButton >
       </div>
       <div className='projects' >
