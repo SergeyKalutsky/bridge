a20c08265a29e56271fcab1b6008611190ce1e90
Kalutsky Sergey <SKalutsky@optimum.businesscar.ru>
2021-11-17 15:38:04 +0300

    deletet not used argumetn

--- desktop-app/src/components/projects/ProjectsMenu.tsx
+++ desktop-app/src/components/projects/ProjectsMenu.tsx
@@ -7,7 +7,7 @@ import IconButton from '@material-ui/core/IconButton';
 import { makeStyles } from '@material-ui/core/styles';
 import '../../assets/css/ProjectsMenu.css'
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(() => ({
   menuIcon: {
     '& svg': {
       fontSize: 30
