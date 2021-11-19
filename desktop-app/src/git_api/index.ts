3622504a8937b4fe972bc5618d360a7ab1402140
SergeyKalutsky <skalutsky@gmail.com>
2021-11-19 19:28:20 +0300

    pulling works too

--- desktop-app/src/git_api/index.ts
+++ desktop-app/src/git_api/index.ts
@@ -3,27 +3,41 @@ const storage = require('electron-json-storage')
 
 const gitDirName = (remote: string, dir: string) => {
   const folders = remote.split('/')
-  dir = dir + '/' + folders[folders.length-1].replace('.git', '')
+  dir = dir + '/' + folders[folders.length - 1].replace('.git', '')
   return dir
 }
 
-const options: Partial<SimpleGitOptions> = {
-  baseDir: storage.getDataPath(),
-  binary: 'git',
-  maxConcurrentProcesses: 6,
-};
+const simplePush = (dir: string) => {
+  const options: Partial<SimpleGitOptions> = {
+    baseDir: dir,
+    binary: 'git',
+    maxConcurrentProcesses: 6,
+  };
 
-const git: SimpleGit = simpleGit();
-
-const pull = (branches: string[]) => {
-  const git: SimpleGit = simpleGit('C:\\Users\\skalutsky\\fluffy-meme', { binary: 'git' });
-  for (const branch of branches) {
-    git.pull('origin', branch)
-  }
+  const git: SimpleGit = simpleGit(options);
+  git.add('./*')
+    .commit('test')
+    .push()
 }
+
+const pull = (dir: string) => {
+  const options: Partial<SimpleGitOptions> = {
+    baseDir: dir,
+    binary: 'git',
+    maxConcurrentProcesses: 6,
+  };
+  const git: SimpleGit = simpleGit(options);
+  git.pull()
+}
+
 
 const dest = 'C:/Users/skalu/AppData/Roaming/my-new-app/storage/'
 const remote = 'https://gitlab.bridgeacross.xyz/sergey/Test22.git'
 
-git.clone(remote, gitDirName(remote, dest))
+// simplePush(gitDirName(remote, dest))
+pull(gitDirName(remote, dest))
+
+
+// git.clone(remote, gitDirName(remote, dest))
+
 
