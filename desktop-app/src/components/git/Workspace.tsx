e652702b5e33a07fe6827f21748cb83c152872c2
SergeyKalutsky <skalutsky@gmail.com>
2021-11-21 11:25:04 +0300

    some cleanup

--- desktop-app/src/components/git/Workspace.tsx
+++ desktop-app/src/components/git/Workspace.tsx
@@ -5,36 +5,14 @@ import WorkspaceTab from './WorkspaceTab'
 import '../../assets/css/Workspace.css'
 import { SimpleGit } from 'simple-git';
 
-const oldCode = `
-const a = 10
-const b = 10
-const c = () => console.log('foo')
-
-if(a > 10) {
-  console.log('bar')
-}
-
-console.log('done')
-`;
-const newCode = `
-const a = 10
-const boo = 10
-
-if(a === 10) {
-  console.log('bar')
-}
-console.log('hello world')
-`;
-
 type GitDiff = {
   filename: string
   newFile: string
   oldFile: string
 }
 
-
 const WorkspaceGit = (): JSX.Element => {
-  const [gitDiff, setGitDiff] = useState<GitDiff>({filename:'', newFile:'', oldFile:''})
+  const [gitDiff, setGitDiff] = useState<GitDiff>({ filename: '', newFile: '', oldFile: '' })
   useEffect(() => {
     const gitDiff = ipcRenderer.sendSync('git-diff', '9c1224f18f2e2642b52831f9f44fb94dca87bdd4')
     setGitDiff(gitDiff[0])
