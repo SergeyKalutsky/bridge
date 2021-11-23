4948ad46361f9e17b63cfc30e249c94af7656abe
Kalutsky Sergey <SKalutsky@optimum.businesscar.ru>
2021-11-23 14:11:12 +0300

    added auth test and some clean up

--- api/tests/tests.py
+++ api/tests/tests.py
@@ -1,23 +1,35 @@
 from ..main import app
 from fastapi.testclient import TestClient
-
+from ..database import sess, t
+from ..security import encode_auth_token
 
 client = TestClient(app)
+TOKEN = encode_auth_token(0)
 
 
 def test_users_find():
+    name = "nik"
     res = client.post(
         '/users/find',
-        headers={'api-key': 'e6b2c842e83c4c01948c792534408e11',
-                 'user-id': '34'},
+        headers={'x-api-key': TOKEN},
         json={
-            "name": "nik"
+            "name": name
         }
     )
     assert res.status_code == 200
-    assert res.json() == [
-        [
-            37,
-            "nikita"
-        ]
-    ]
+    q = sess.query(t.Users.id, t.Users.name).\
+        filter(t.Users.name.contains(f'%{name}%')).all()
+    sample_data = [list(i) for i in q]
+    assert res.json() == sample_data
+
+
+def test_auth():
+    res = client.post(
+        '/auth',
+        json={
+            "password": "rjyrehc1",
+            "login": "root"
+        }
+    )
+    assert res.status_code == 200
+    assert res.json()['status'] == 'success'
