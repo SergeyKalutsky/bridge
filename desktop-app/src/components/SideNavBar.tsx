9a868155bc39b713c2adcd499d17bb1963d0550c
Sergey Kalutsky <skalutsky@gmail.com>
2021-11-25 13:33:20 +0300

    fixed errors with git for not active projects

--- desktop-app/src/components/SideNavBar.tsx
+++ desktop-app/src/components/SideNavBar.tsx
@@ -4,8 +4,9 @@ import FileCopyIcon from '@material-ui/icons/FileCopy';
 import { makeStyles } from '@material-ui/core/styles';
 import ClassIcon from '@material-ui/icons/Class';
 import IconButton from '@material-ui/core/IconButton';
-import { ipcRenderer } from "electron";
 import { Link } from 'react-router-dom'
+import { useState } from 'react';
+import Popup from 'reactjs-popup';
 import '../assets/css/SideNavBar.css'
 
 const useStyles = makeStyles((theme) => ({
@@ -19,8 +20,23 @@ const useStyles = makeStyles((theme) => ({
 })
 );
 
+const HelperMessage = (): JSX.Element => {
+    return (
+        <Popup
+            trigger={<div className='icon'><AccountTreeIcon /></div>}
+            position="right center"
+        >
+            {
+                <div>Чтобы использовать Git нужны выбрать или создать проект</div>
+            }
+        </Popup >
+
+    )
+}
+
 
 const SideNavBar = (): JSX.Element => {
+    const [activeProjectSet, setActiveProjectSet] = useState(false)
     const classes = useStyles();
 
     return (
@@ -31,8 +47,14 @@ const SideNavBar = (): JSX.Element => {
             <IconButton className={classes.menuIcon}>
                 <FileCopyIcon />
             </IconButton>
-            <IconButton className={classes.menuIcon} onClick={()=>ipcRenderer.removeAllListeners('stdout')}>
-                <Link to="/git" replace><AccountTreeIcon /></Link>
+            <IconButton className={classes.menuIcon} onClick={() => {
+                const settings = JSON.parse(window.sessionStorage.getItem('settings'))
+                'active_project' in settings ? setActiveProjectSet(true) : setActiveProjectSet(false)
+            }
+            }>
+                {activeProjectSet == true ?
+                    <Link to="/git" replace><AccountTreeIcon /></Link> :
+                    <HelperMessage />}
             </IconButton>
             <IconButton className={classes.menuIcon}>
                 <Link to="/" replace><ClassIcon /></Link>
