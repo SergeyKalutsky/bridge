74803e3f219e7d9708cd8cef97b091bf805b750b
SergeyKalutsky <skalutsky@gmail.com>
2021-12-05 12:35:03 +0300

    added some planed tests

--- api/tests/tests.py
+++ api/tests/tests.py
@@ -1,10 +1,11 @@
 from ..main import app
 from fastapi.testclient import TestClient
-from ..database import sess, t
 from ..security import encode_auth_token
+from ..queries import find_user_by_name
 
 client = TestClient(app)
-TOKEN = encode_auth_token(0)
+USER_ID = 0
+TOKEN = encode_auth_token(USER_ID)
 
 
 def test_users_find():
@@ -17,10 +18,8 @@ def test_users_find():
         }
     )
     assert res.status_code == 200
-    q = sess.query(t.Users.id, t.Users.name).\
-        filter(t.Users.name.contains(f'%{name}%')).all()
-    sample_data = [list(i) for i in q]
-    assert res.json() == sample_data
+    q = find_user_by_name(name, USER_ID)
+    assert res.json() == [{'id': i.id, 'name': i.name} for i in q]
 
 
 def test_auth():
@@ -28,8 +27,19 @@ def test_auth():
         '/auth',
         json={
             "password": "rjyrehc1",
-            "login": "root"
+            "login": "sergey"
         }
     )
     assert res.status_code == 200
-    assert res.json()['status'] == 'success'
+
+
+def test_list_members():
+    pass
+
+
+def test_add_member():
+    pass
+
+
+def test_delete_member():
+    pass
