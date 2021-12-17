8f61214148aca1d9252b55cff9d268af0447c895
Sergey Kalutsky <skalutsky@gmail.com>
2021-12-17 11:35:40 +0300

    make file icons look clickable

--- bridge/src/components/Editor/tree_viewer/TreeTab.tsx
+++ bridge/src/components/Editor/tree_viewer/TreeTab.tsx
@@ -1,9 +1,15 @@
+import { IconContext } from "react-icons";
+import { AiOutlineFileAdd, AiOutlineFolderAdd, AiFillDelete } from "react-icons/ai";
 
 const TreeTab = (): JSX.Element => {
     return (
         <div className="tree-tab">
-
-            </div>
+            <IconContext.Provider value={{ color: 'navy', size: '25', className:'file-icon' }}>
+                <AiOutlineFileAdd />
+                <AiOutlineFolderAdd />
+                <AiFillDelete />
+            </IconContext.Provider>
+        </div>
     )
 }
 
