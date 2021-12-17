3268dfe44fdb5e3109f7621ce56d6223f7581995
Sergey Kalutsky <skalutsky@gmail.com>
2021-12-17 14:09:17 +0300

    getting project name works

--- bridge/src/components/Editor/tree_viewer/TreeTab.tsx
+++ bridge/src/components/Editor/tree_viewer/TreeTab.tsx
@@ -14,15 +14,16 @@ interface Props {
 }
 
 const TreeTab = ({ activePath, forceUpdate }: Props): JSX.Element => {
+    const settigns = window.settings.get()
     return (
         <div className="tree-tab">
             <div className="project-name">
-                XXXXX
+                {settigns.active_project.name}
             </div>
             <div className="file-icons">
                 <IconContext.Provider value={{ color: 'navy', size: '25', className: 'file-icon' }}>
-                    <NewFile activePath={activePath} forceUpdate={forceUpdate}/>
-                    <NewFolder activePath={activePath} forceUpdate={forceUpdate}/>
+                    <NewFile activePath={activePath} forceUpdate={forceUpdate} />
+                    <NewFolder activePath={activePath} forceUpdate={forceUpdate} />
                     <AiFillDelete />
                 </IconContext.Provider>
             </div>
