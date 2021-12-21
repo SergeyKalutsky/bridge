e634bcb959f55d0cba7bbed094958a894d22cfe5
Sergey Kalutsky <skalutsky@gmail.com>
2021-12-21 11:55:40 +0300

    refactored trash icon a bit

--- bridge/src/components/projects/icons/TrashIconButton.tsx
+++ bridge/src/components/projects/icons/TrashIconButton.tsx
@@ -1,4 +1,5 @@
-import Popup from 'reactjs-popup';
+import PopUp from '../../common/PopUp';
+import Button from '../../common/Button';
 import { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTrashAlt } from '@fortawesome/free-solid-svg-icons'
@@ -13,40 +14,33 @@ type Props = {
 
 const TrashIconButton = ({ name, id, removeProject, setActive }: Props): JSX.Element => {
     const [open, setOpen] = useState(false)
+    const handleClick = () => {
+        const settings = window.settings.get()
+        if (settings.active_project !== undefined && settings.active_project.id === id) {
+            settings.active_project = undefined;
+            console.log(settings)
+            window.settings.set(settings)
+        }
+        deleteProject(settings, id)
+        window.projects.delete(name)
+        removeProject(id)
+        setOpen(false)
+    }
+    const handleClose = () => {
+        setOpen(false)
+        setActive(false)
+    }
     return (
         <>
-            <div className='icon' onClick={() => {
-                setOpen(true)
-            }}>
-                <FontAwesomeIcon icon={faTrashAlt} /></div>
-            <Popup
+            <div className='icon' onClick={() => { setOpen(true) }}>
+                <FontAwesomeIcon icon={faTrashAlt} />
+            </div>
+            <PopUp
                 open={open}
-                position="right center"
-                modal
-                closeOnDocumentClick
-                onClose={() => {
-                    setOpen(false)
-                    setActive(false)
-                }}
-            >
-                <div className="modal">
-                    <div>Вы уверены, что хотите удалить/покинуть проект? (Изменения необратимы)</div>
-                    <button className="close danger" onClick={() => {
-                        const settings = window.settings.get()
-                        if (settings.active_project !== undefined && settings.active_project.id === id){
-                            settings.active_project = undefined;
-                            console.log(settings)
-                            window.settings.set(settings)
-                        }
-                        deleteProject(settings, id)
-                        window.projects.delete(name)
-                        removeProject(id)
-                        setOpen(false)
-                    }}>
-                        Удалить
-                    </button>
-                </div>
-            </Popup >
+                onClose={handleClose}>
+                <div>Вы уверены, что хотите удалить/покинуть проект? (Изменения необратимы)</div>
+                <Button onClick={handleClick} btnText='Удалить' />
+            </PopUp>
         </>
 
     )
