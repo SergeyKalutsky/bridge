542008829a15266c2dac5aefdea74bd45d66fede
Sergey Kalutsky <skalutsky@gmail.com>
2021-12-21 14:22:14 +0300

    fixed on mouse over

--- bridge/src/components/projects/ProjectItem.tsx
+++ bridge/src/components/projects/ProjectItem.tsx
@@ -14,19 +14,20 @@ type Props = {
 }
 
 
-const ProjectDiv = ({ children, project, onClick, dispatch, removeProject }) => {
+const ProjectDiv = ({ children, project, handleClick, dispatch, removeProject }) => {
     const [active, setActive] = useState(false)
     const activeProject = project.isactive ? 'border-l-4 border-zinc-50' : ''
     const localProject = project.islocal ? '' : 'after:content-["🔥"] opacity-30'
+    console.log(active)
     return (
         <div className={`${activeProject} ${localProject} 
                 mt-2 h-18 flex justify-between items-center 
                 text-3xl cursor-pointer hover:bg-slate-500`}
-            onMouseOver={() => { project.islocal ? setActive(true) : null }}
+            onMouseEnter={() => { project.islocal ? setActive(true) : null }}
             onMouseLeave={() => { project.islocal ? setActive(false) : null }}
             onClick={() => { setActive(false) }}>
             <span className='ml-5 font-medium text-white w-full'
-                onClick={onClick}>
+                onClick={() => handleClick(project)}>
                 {project.name}
             </span>
             {children}
@@ -55,13 +56,11 @@ const ProjectItem = ({ project,
         setOpenSelectActive(false)
         setActiveProject(active_project.id)
     }
-    const handleClick = (e) => {
-        e.currentTarget.className.includes('non-active') == true ?
-            setOpenActivate(true) :
-            setOpenSelectActive(true)
+    const handleClick = (project) => {
+        !project.islocal ? setOpenActivate(true) : setOpenSelectActive(true)
     }
     return (
-        <ProjectDiv onClick={handleClick} project={project} dispatch={dispatch} removeProject={removeProject}>
+        <ProjectDiv handleClick={handleClick} project={project} dispatch={dispatch} removeProject={removeProject}>
             <SelectActiveProject
                 project={project}
                 setOpen={setOpenSelectActive}
