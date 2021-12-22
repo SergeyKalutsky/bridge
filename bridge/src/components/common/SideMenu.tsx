e9bede7cde330298bb09e2d0143940480b1361aa
Sergey Kalutsky <skalutsky@gmail.com>
2021-12-22 09:53:24 +0300

    draggable kind of works

--- bridge/src/components/common/SideMenu.tsx
+++ bridge/src/components/common/SideMenu.tsx
@@ -1,32 +1,36 @@
-import { useState, useRef, useCallback, useEffect } from 'react'
+import { useState, useRef, useCallback } from 'react'
+import React from 'react'
 
 interface Props {
     children: React.ReactNode
 }
 
-const SideMenu = ({ children }: Props): JSX.Element => {
-    const [width, setWidth] = useState(300);
-    const ref = useRef<HTMLButtonElement>();
 
+const SideMenu = ({ children }: Props): JSX.Element => {
+    const [size, setSize] = useState(300);
+    const ref = useRef<HTMLButtonElement>();
     const handler = useCallback(() => {
         function onMouseMove(e) {
-            setWidth(400);
-            console.log(width)
+            console.log(e.pageX, e.pageY)
+            if (!(e.pageX <= 350) && !(e.pageX >= 600)) {
+                setSize(e.pageX - 80)
+            }
         }
         function onMouseUp() {
-            ref.current.removeEventListener("mousemove", onMouseMove);
-            ref.current.addEventListener("mouseup", onMouseUp);
+            window.removeEventListener("mousemove", onMouseMove);
+            window.removeEventListener("mouseup", onMouseUp);
         }
-        ref.current.addEventListener("mousemove", onMouseMove);
-        ref.current.addEventListener("mouseup", onMouseUp);
+        window.addEventListener("mousemove", onMouseMove);
+        window.addEventListener("mouseup", onMouseUp);
     }, []);
+
     return (
         <>
-            <div className={`w-1/4 h-full bg-zinc-800 drop-shadow-lg`}
-                style={{ width: width }}>
+            <div className={`h-full bg-zinc-800 drop-shadow-lg`}
+                style={{ width: size }}>
                 {children}
             </div>
-            <button className='w-10 h-full cursor-col-resize' ref={ref} onMouseDown={handler} />
+            <button className='w-1 h-full cursor-col-resize' ref={ref} onMouseDown={handler} />
         </>
     )
 }
