49d2395534b6b798d55df9f5e915e0ab75b4e230
Sergey Kalutsky <skalutsky@gmail.com>
2021-12-24 14:11:39 +0300

    refactored resize a bit

--- bridge/src/components/Editor/XtermTerminal.tsx
+++ bridge/src/components/Editor/XtermTerminal.tsx
@@ -19,7 +19,10 @@ const XtermTerminal = ({ activePath, activeToggle }: Props): JSX.Element => {
     const [size, setSize] = useState(1000);
     const ref = useRef<HTMLDivElement>();
 
-    const handler = () => {
+    useEffect(() => {
+        if (!activeToggle) {
+            return
+        }
         function onMouseMove(e) {
             setSize(size => size - e.movementX)
             fitAddon.fit()
@@ -30,13 +33,6 @@ const XtermTerminal = ({ activePath, activeToggle }: Props): JSX.Element => {
         }
         window.addEventListener("mousemove", onMouseMove);
         window.addEventListener("mouseup", onMouseUp);
-    }
-
-    useEffect(() => {
-        if (!activeToggle) {
-            return
-        }
-        handler()
     }, [activeToggle])
 
 
