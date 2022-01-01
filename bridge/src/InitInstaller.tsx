86723fe59de6bf08a4154e6b4e2ac5991ca6b90d
SergeyKalutsky <skalutsky@gmail.com>
2022-01-01 13:24:22 +0300

    main ui fronted for initinstall is done

--- bridge/src/InitInstaller.tsx
+++ bridge/src/InitInstaller.tsx
@@ -1,13 +1,23 @@
 import { Button } from "./components/common"
-
+import { LogoIcon } from './components/common/Icons';
 
 const InitInstaller = (): JSX.Element => {
+    const checkMark = <>✔️</>
+    const crossMark = <>❌</> 
     const handleClick = () => { window.pkg.checkInstall('git') }
     return (
-        <div className="w-full h-full flex items-center justify-center bg-slate-500">
-            <Button onClick={handleClick}>
-                Установить
-            </Button>
+        <div className="w-full h-full flex flex-col gap-10 items-center justify-center bg-slate-800">
+            <LogoIcon />
+            <div className="w-full h-2/4 flex flex-col items-center gap-10">
+                <h1 className="text-white font-medium text-3xl">Для работы BRIDGE требуется установка choco и git</h1>
+                <div className="flex flex-col">
+                    <span className="text-white font-medium text-xl">{checkMark}Chocolotey</span>
+                    <span className="text-white font-medium text-xl">{crossMark}Git</span>
+                </div>
+                <Button onClick={handleClick}>
+                    Установить
+                </Button>
+            </div>
         </div>
     )
 }
