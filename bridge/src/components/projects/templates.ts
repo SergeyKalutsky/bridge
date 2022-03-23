1d66321efd188ebc2c5cbcf8475ab73bc63ee5e4
Sergey Kalutsky <skalutsky@gmail.com>
2022-03-23 16:19:57 +0300

    fixed templates a bit

--- bridge/src/components/projects/templates.ts
+++ bridge/src/components/projects/templates.ts
@@ -1,22 +1,19 @@
 const templates = {
     'Python': {
-        apps: ['python'],
+        lib: ['python'],
         http: 'https://github.com/SergeyKalutsky/python_base.git',
     },
     'Python Flask' : {
-        apps: ['python'],
+        lib: ['python', 'flask', 'flask_login', 'sqlalchemy'],
         http: 'https://github.com/SergeyKalutsky/flask_base.git',
-        cmd: 'pip install -r requirements.txt'
     },
     'Python Discord' : {
-        apps: ['python'],
+        lib: ['python', 'discord.py'],
         http: 'https://github.com/SergeyKalutsky/discord_template.git',
-        cmd: 'pip install -r requirements.txt'
     },
     'Python Pgzero' : {
-        apps: ['python'],
+        lib: ['python', 'pgzero'],
         http: 'https://github.com/SergeyKalutsky/pgzero_template.git',
-        cmd: 'pip install -r requirements.txt'
     }
 
 }
--- bridge/src/lib/pkg_manager/cmds.ts
+++ bridge/src/lib/pkg_manager/cmds.ts
@@ -1,5 +1,6 @@
 const CMD = {
     git: {
+        elevate: true,
         install: {
             linux: "sudo apt install git",
             win32: "choco install -y git"
@@ -12,13 +13,8 @@ const CMD = {
             win32: ';C:\\Program Files\\Git\\cmd'
         }
     },
-    vscode: {
-        install: {
-            linux: "sudo snap install --classic code",
-            win32: "choco install -y vscode"
-        }
-    },
     python: {
+        elevate: true,
         install: {
             linux: "sudo apt-get install python3.10",
             win32: "choco install -y python3 --version=3.10.1"
@@ -29,6 +25,7 @@ const CMD = {
         }
     },
     choco: {
+        elevate: true,
         install: {
             win32: "Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString(''https://community.chocolatey.org/install.ps1''))"
         },
@@ -38,6 +35,48 @@ const CMD = {
         },
         path: {
             win32: ';C:\\ProgramData\\chocolatey\\bin'
+        }
+    },
+    pgzero: {
+        elevate: false,
+        install: {
+            win32: 'pip install pgzero --user',
+            darwin: 'pip install pgzero'
+        }
+    },
+    'discord.py': {
+        elevate: false,
+        install: {
+            win32: 'pip install discord.py --user',
+            darwin: 'pip install discord.py'
+        }
+    },
+    requests: {
+        elevate: false,
+        install: {
+            win32: 'pip install requests --user',
+            darwin: 'pip install requests'
+        }
+    },
+    flask_login: {
+        elevate: false,
+        install: {
+            win32: 'pip install flask_login --user',
+            darwin: 'pip install flask_login'
+        }
+    },
+    sqlalchemy: {
+        elevate: false,
+        install: {
+            win32: 'pip install sqlalchemy --user',
+            darwin: 'pip install sqlalchemy'
+        }
+    },
+    flask: {
+        elevate: false,
+        install: {
+            win32: 'pip install flask --user',
+            darwin: 'pip install flask'
         }
     }
 }
