5734fb50d203e8182e4cde1ea0aec2b685699686
Sergey Kalutsky <skalutsky@gmail.com>
2022-04-01 11:21:39 +0300

    added highlight for dragging onto folder

--- bridge/src/components/Editor/tree_viewer/Folder.tsx
+++ bridge/src/components/Editor/tree_viewer/Folder.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import { ActivePath } from "../types";
 import { AiOutlineFolder } from "react-icons/ai";
 
@@ -12,16 +12,40 @@ interface Props {
 
 const Folder = ({ name, children, path, activePath, setActivePath }: Props): JSX.Element => {
   const [isOpen, setIsOpen] = useState(false);
-
+  const [color, setColor] = useState('bg-transperent')
+  const ref = useRef(null)
   const handleToggle = e => {
     setActivePath({ path: path, isDirectory: true })
     e.preventDefault()
     setIsOpen(!isOpen)
   };
-  const height = isOpen ? "h-0" : "h-auto"
+
+  useEffect(() => {
+    const onDragEnter = (e) => {
+      console.log('File is in the Drop Space')
+      setColor("bg-slate-700")
+    }
+    ref.current.addEventListener('dragenter', onDragEnter)
+    return () => ref.current.removeEventListener('dragenter', onDragEnter)
+  }, [])
+
+  useEffect(() => {
+    const onDragLeave = (e) => {
+      console.log('File has left the Drop Space')
+      setColor("bg-transperent")
+    }
+    ref.current.addEventListener('dragleave', onDragLeave)
+    return () => ref.current.removeEventListener('dragleave', onDragLeave)
+  }, [])
+
+  useEffect(() => {
+    setColor(bgColor)
+  }, [])
+  
   const bgColor = activePath !== null && path === activePath.path ? "bg-slate-700" : "bg-transperent"
+  const height = isOpen ? "h-0" : "h-auto"
   return (
-    <div className="pl-[20px]">
+    <div ref={ref} className={`pl-[20px] ${color}`}>
       <div className={`${bgColor} flex items-center hover:bg-slate-700 hover:cursor-pointer`}
         onClick={handleToggle}>
         <span className="text-white"><AiOutlineFolder /></span>
