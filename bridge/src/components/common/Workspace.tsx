eeac4eb1704994386f62382181c6b4370a04607f
Sergey Kalutsky <skalutsky@gmail.com>
2022-04-04 17:24:21 +0300

    fixed issue with  git diff size

--- bridge/src/components/common/Workspace.tsx
+++ bridge/src/components/common/Workspace.tsx
@@ -4,7 +4,7 @@ interface Props {
 
 const Workspace = ({ children }: Props): JSX.Element => {
     return (
-        <div className='h-full border-l-1 grow bg-neutral-900 flex flex-col'>
+        <div className='h-full w-[calc(100%-430px)] border-l-1 grow bg-neutral-900 flex flex-col'>
             {children}
         </div>
     )
