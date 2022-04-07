7f8575860cd02487aebb0a89d02108423f6bb519
Sergey Kalutsky <skalutsky@gmail.com>
2022-04-07 15:56:33 +0300

    removed useless file

--- bridge/src/lib/api/main/test.ts
+++
@@ -1,9 +0,0 @@
-import fs from 'fs'
-import path from 'path'
-
-
-const filepath = '/path/to'
-console.log(path.parse(filepath).dir)
-
-
-// fs.renameSync('/path/to/Afghanistan.png', '/path/to/AF.png');
