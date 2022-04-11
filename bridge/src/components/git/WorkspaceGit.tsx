4552f5e3a77f88fbe7abb5cc0632ff786e1553ef
Sergey Kalutsky <skalutsky@gmail.com>
2022-04-11 11:42:51 +0300

    deleted useless import useref

--- bridge/src/components/git/WorkspaceGit.tsx
+++ bridge/src/components/git/WorkspaceGit.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useState } from 'react';
 import Workspace from '../common/Workspace'
 import ReactDiffViewer from 'react-diff-viewer';
 import ToolBar from '../common/ToolBar'
