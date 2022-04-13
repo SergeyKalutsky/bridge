8626ab695d6fdaf220a4c38061fc4fb70e111094
Sergey Kalutsky <skalutsky@gmail.com>
2022-04-13 13:09:08 +0300

    added neat border for commits selection

--- bridge/src/components/git/CommitRow.tsx
+++ bridge/src/components/git/CommitRow.tsx
@@ -6,9 +6,9 @@ type Props = {
 
 
 const CommitRow = ({ hash, active, onClickCallback }: Props): JSX.Element => {
-    const hashActive = active ? '': ''
+    const hashActive = active ? 'border-l-4 border-zinc-50' : ''
     return (
-        <div className='hover:bg-slate-700 hover:cursor-pointer text-xl flex items-center justify-center py-2 h-[40px] w-full'
+        <div className={`${hashActive} hover:bg-slate-700 hover:cursor-pointer text-xl flex items-center justify-center py-2 h-[40px] w-full`}
             onClick={() => onClickCallback(hash)}
         >
             <span className='text-white mr-[20px]'>commit</span>
