0651eab9882f41c03fd4541951eebaa8477d6569
Sergey Kalutsky <skalutsky@gmail.com>
2023-01-03 23:03:58 +0600

    almost wrote a revert function

--- bridge/src/lib/git/index.ts
+++ bridge/src/lib/git/index.ts
@@ -37,51 +37,113 @@ function Utf8ArrayToStr(array) {
     return out;
 }
 
-const getFileChanges = async (oid: string, oid_prev: string) => {
+
+// const getFileChanges = async (oid: string, oid_prev: string) => {
+//     const A = git.TREE({ ref: oid })
+//     const B = git.TREE({ ref: oid_prev })
+//     console.log('here', A, B, oid, oid_prev)
+//     // Get a list of the files that changed
+//     const fileChanges = [];
+//     await git.walk({
+//         fs: fs,
+//         dir: '/Users/sergeykalutsky/Library/Application Support/bridge/storage/guest/test_flask',
+//         trees: [A, B],
+//         map: async function (filename, [A, B]) {
+//             if (await A.type() === 'tree') return
+
+//             if (B === null) {
+//                 const Auint8array = await A.content()
+//                 fileChanges.push({
+//                     filename: filename,
+//                     newFile: Utf8ArrayToStr(Auint8array),
+//                     oldFile: ''
+//                 })
+//                 return
+//             }
+
+//             const Auint8array = await A.content()
+//             const Buint8array = await B.content()
+//             console.log(Auint8array, Buint8array)
+
+//             fileChanges.push({
+//                 filename: filename,
+//                 newFile: Utf8ArrayToStr(Auint8array),
+//                 oldFile: Utf8ArrayToStr(Buint8array)
+//             })
+
+//         }
+//     })
+//     return fileChanges
+// }
+
+function cleanEmptyFoldersRecursively(folder) {
+    const isDir = fs.statSync(folder).isDirectory();
+    if (!isDir) {
+        return;
+    }
+    let files = fs.readdirSync(folder);
+    if (files.length > 0) {
+        files.forEach(function (file) {
+            const fullPath = path.join(folder, file);
+            cleanEmptyFoldersRecursively(fullPath);
+        });
+
+        // re-evaluate files; after deleting subfolder
+        // we may have parent folder empty now
+        files = fs.readdirSync(folder);
+    }
+    if (files.length == 0) {
+        console.log("removing: ", folder);
+        fs.rmdirSync(folder);
+        return;
+    }
+}
+
+const revertChanges = async (oid: string, oid_revert: string, dir: string) => {
     const A = git.TREE({ ref: oid })
-    const B = git.TREE({ ref: oid_prev })
-    console.log('here', A, B, oid, oid_prev)
-    // Get a list of the files that changed
-    const fileChanges = [];
-    await git.walk({
-        fs: fs,
-        dir: '/Users/sergeykalutsky/Library/Application Support/bridge/storage/guest/test_flask',
-        trees: [A, B],
-        map: async function (filename, [A, B]) {
-            if (await A.type() === 'tree') return
-            
-            if (B === null) {
-                const Auint8array = await A.content()
-                fileChanges.push({
-                    filename: filename,
-                      newFile: Utf8ArrayToStr(Auint8array),
-                      oldFile: ''
-                })
-                return 
+    const B = git.TREE({ ref: oid_revert })
+    dir = '/Users/sergeykalutsky/Library/Application Support/bridge/storage/guest/test_flask',
+        await git.walk({
+            fs: fs,
+            dir: dir,
+            trees: [A, B],
+            map: async function (filename, [A, B]) {
+
+                const filePath = path.join(dir, filename)
+                const dirPath = path.dirname(filePath)
+
+                if (A === null) {
+                    if (!fs.existsSync(dirPath)) {
+                        fs.mkdir(dirPath, { recursive: true }, async (err) => {
+                            if (err) throw err;
+                        });
+                    }
+                    const oldContent = Utf8ArrayToStr(await B.content())
+                    fs.writeFile(filePath, oldContent, 'utf8', function (err) {
+                        if (err) return console.log(err);
+                    });
+                    return
+                }
+
+                if (await A.type() === 'tree') return
+
+                if (B === null) {
+                    fs.unlinkSync(filePath);
+                    return
+                }
+                const oldContent = Utf8ArrayToStr(await B.content())
+                fs.writeFile(filePath, oldContent, 'utf8', function (err) {
+                    if (err) return console.log(err);
+                });
             }
-            const Aoid = await A.oid();
-            const Boid = await B.oid();
-            const Auint8array = await A.content()
-            const Buint8array = await B.content()
-            console.log(Auint8array, Buint8array)
-
-            // Skip pairs where the oids are the same
-            if (Aoid === Boid) return
-
-            fileChanges.push({
-                filename: filename,
-                  newFile: Utf8ArrayToStr(Auint8array),
-                  oldFile: Utf8ArrayToStr(Buint8array)
-            })
-
-        }
-    })
-    return fileChanges
+        })
+    cleanEmptyFoldersRecursively(dir)
 }
 
 const main = async () => {
-    const res = await getFileChanges('4915ab99d7a8c80c7296b48268490cad468098ec', '')
-    console.log(res)
+    const current = '422f6974b491415345c8bd79b2101fa885d63b42'
+    const previous = '559ebac92ba8bcb963642c626f3e6a55fe5fabdd'
+    await revertChanges(current, previous, '')
 }
 main()
 
