b4d32e5d444ef7bf44a68744d3bd6831a5258861
Sergey Kalutsky <skalutsky@gmail.com>
2023-01-05 17:32:09 +0600

    clean up

--- bridge/src/components/git/GitMenu.tsx
+++ bridge/src/components/git/GitMenu.tsx
@@ -24,8 +24,6 @@ const GitMenu = ({ git, setGit }: Props): JSX.Element => {
     const oid_prev = git.oids[i] === undefined ? '' : git.oids[i]
     const args = {oid: oid, oid_prev: oid_prev}
     const gitDiffs = await window.git.diff(args)
-
-    console.log(oid)
     setGit({
       ...git,
       gitDiffs: gitDiffs,
