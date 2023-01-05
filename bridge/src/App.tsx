44324a683958fabca508d60dbc3ce1bc7237df45
Sergey Kalutsky <skalutsky@gmail.com>
2023-01-05 18:14:01 +0600

    removed init installer

--- bridge/src/App.tsx
+++ bridge/src/App.tsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from 'react';
-import InitInstaller from './InitInstaller'
 import LoginPage from './components/login/LoginPage';
 import AppContent from './components/AppContent'
 import './assets/index.css'
@@ -7,7 +6,6 @@ import './assets/index.css'
 
 export default function App(): JSX.Element {
     const [islogin, setIslogin] = useState(false)
-    const [isFirstLoad, setIsFirstLoad] = useState(true)
     const [userSettingsLoaded, setUserSettingLoaded] = useState(false)
     const [content, setContent] = useState<JSX.Element>(null)
 
@@ -15,22 +13,17 @@ export default function App(): JSX.Element {
         const user = window.settings.get('user')
         setUserSettingLoaded(true)
         user !== undefined ? setIslogin(false) : setIslogin(true)
-        user !== undefined ? setIsFirstLoad(false) : setIsFirstLoad(true)
     }, [])
 
     useEffect(() => {
         if (userSettingsLoaded) {
-            if (isFirstLoad) {
-                setContent(<InitInstaller setIsFirstLoad={setIsFirstLoad} />)
-                return
-            }
             if (islogin) {
                 setContent(<LoginPage setIslogin={setIslogin} />)
                 return
             }
             setContent(<AppContent />)
         }
-    }, [userSettingsLoaded, isFirstLoad, islogin])
+    }, [userSettingsLoaded, islogin])
 
     return <>{content}</>
 }
