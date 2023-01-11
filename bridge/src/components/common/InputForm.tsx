3322c0e7eb75c27450373d748066223cb803398a
Sergey Kalutsky <skalutsky@gmail.com>
2023-01-12 00:51:46 +0600

    more changes to imput form and made an input

--- bridge/src/components/common/InputForm.tsx
+++ bridge/src/components/common/InputForm.tsx
@@ -3,7 +3,8 @@ interface Props {
     placeholder: string
     type: string
     value?: string
-    className?: string,
+    classInput?: string,
+    classDiv?: string,
     handleKeyPress?: (e: React.KeyboardEvent<HTMLInputElement>) => void
     onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void
 }
@@ -12,20 +13,22 @@ export default function InputForm({ children,
     placeholder,
     type,
     value,
-    className = '',
+    classInput = '',
+    classDiv = '',
     handleKeyPress,
     onChange }: Props): JSX.Element {
-        const handleFocus = (event: React.ChangeEvent<HTMLInputElement>) => {
-            if (value === undefined)
+    const handleFocus = (event: React.ChangeEvent<HTMLInputElement>) => {
+        if (value === undefined)
             return
-            event.target.value = value
-            event.target.setSelectionRange(0, event.target.value.lastIndexOf('.'))
-        }
-    const defaultClass = 'bg-zinc-300 pl-3 pr-3 pt-2 pb-2 text-xl w-full text-slate-700 placeholder-slate-500 border border-neutral-400 rounded-lg focus:outline-none'
+        event.target.value = value
+        event.target.setSelectionRange(0, event.target.value.lastIndexOf('.'))
+    }
+    const defaultClassInput = 'bg-zinc-300 pl-3 pr-3 pt-2 pb-2 text-xl w-full text-slate-700 placeholder-slate-500 border border-neutral-400 rounded-lg focus:outline-none '
+    const defaultClassDiv = 'flex items-center justify-start bg-white w-full rounded-lg focus:outline-none bg-zinc-300 '
     return (
-        <div className='flex items-center justify-start bg-white w-full rounded-lg focus:outline-none'>
+        <div className={defaultClassDiv + classDiv}>
             {children}
-            <input className={defaultClass + className}
+            <input className={defaultClassInput + classInput}
                 type={type}
                 placeholder={placeholder}
                 onChange={onChange}
--- bridge/src/components/projects/create/ProjectsSelectType.tsx
+++ bridge/src/components/projects/create/ProjectsSelectType.tsx
@@ -1,11 +1,17 @@
+import { BiSearch } from 'react-icons/bi'
 import { InputForm } from '../../../components/common'
 import { createProjectProp } from './types'
 
 
 export function ProjectsSelectType({ projectCreate, setProjectCreate, setDisabled }: createProjectProp) {
     return (
-        <><InputForm
-            placeholder='Введите тип проетка'
-            type='text'></InputForm></>
+        <>
+            <InputForm
+                placeholder='Введите тип проетка'
+                type='text'
+                classInput='border-none pl-1'>
+                {<BiSearch style={{ color: '#71717a', width: 28, height: 30, paddingLeft: 5, backgroundColor: '#d4d4d8' }} />}
+            </InputForm>
+        </>
     )
 }
