937b56c9b83689b54dd43816b65474e0a4ca35c0
Sergey Kalutsky <skalutsky@gmail.com>
2023-01-14 16:50:44 +0600

    added logic to check if onChange works

--- bridge/src/components/projects/create/ProjectsGithub.tsx
+++ bridge/src/components/projects/create/ProjectsGithub.tsx
@@ -1,5 +1,5 @@
+import { LoadingIcon } from '../../../components/common/Icons'
 import { InputForm } from '../../../components/common'
-import { ImCross } from 'react-icons/im'
 import { createProjectProp } from './types'
 
 
@@ -26,16 +26,36 @@ function WarningMessage({ text }: { text: string }): JSX.Element {
     )
 }
 
+function LoadingMessage({ text }: { text?: string }): JSX.Element {
+    return (
+        <div className='text-xl  w-full flex justify-start items-center mt-3 pt-2 pb-2 pr-2 pl-2'>
+            <LoadingIcon />
+            <p className='font-medium text-slate-100 pl-2'>{text}</p>
+        </div>
+    )
+}
+
 export function ProjectsGithub({ projectCreate, setProjectCreate, setDisabled }: createProjectProp): JSX.Element {
+    const onChange = (e)=> {
+        console.log(e.target.value)
+        if (e.target.value === '') {
+            setDisabled(false)
+            return  
+        } 
+        setDisabled(true)
+
+    }
     return (<>
         <div className='w-full bg-teal-200/60 flex flex-col justify-center items-start mb-8 rounded-lg pt-2 pb-2 pr-2 pl-2'>
             <p className='pl-2'>Этот шаг <span className='font-bold'>необязателен</span>.</p>
             <p className='pl-2'>Вам потребуется <span className='font-bold'>github аккаунт</span> и <span className='font-bold'>токен</span> (<LinkText text='classic' /> или <LinkText text='fain-grain' />).</p>
         </div>
         <InputForm
+            onChange={onChange}
             placeholder='GitHub token'
             type='password'
-            classInput='border-none pl-5 text-2xl text-security-disc'/>
-        <ErrorMessage text='Токен не существует или его срок действия истек' />
+            classInput='border-none pl-5 text-2xl text-security-disc' />
+        {/* <ErrorMessage text='Токен не существует или его срок действия истек' /> */}
+        {/* <LoadingMessage text='Проверка токена'/> */}
     </>)
 }
