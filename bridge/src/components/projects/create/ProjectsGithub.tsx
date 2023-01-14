168b788f997c20c8d9bf227c8aa73da680313e45
SergeyKalutsky <skalutsky@gmail.com>
2023-01-14 11:16:15 +0300

    fixed error icon for windows

--- bridge/src/components/projects/create/ProjectsGithub.tsx
+++ bridge/src/components/projects/create/ProjectsGithub.tsx
@@ -12,7 +12,7 @@ function LinkText({ text }: { text: string }): JSX.Element {
 function ErrorMessage({ text }: { text: string }): JSX.Element {
     return (
         <div className='w-full bg-rose-400/70 flex justify-start items-center mt-8 rounded-lg pt-2 pb-2 pr-2 pl-2'>
-            <p className='font-semibold pl-2'>{'🛑'}</p>
+            <p className='font-medium pl-2'>{'🛑'}</p>
             <p className='font-medium text-slate-100 pl-2'>{text}</p>
         </div>
     )
