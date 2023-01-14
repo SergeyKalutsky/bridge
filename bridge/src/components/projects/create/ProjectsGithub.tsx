46c2f47c001b182b76fefe259393738f911127a4
Sergey Kalutsky <skalutsky@gmail.com>
2023-01-14 13:08:20 +0600

    fixed error message

--- bridge/src/components/projects/create/ProjectsGithub.tsx
+++ bridge/src/components/projects/create/ProjectsGithub.tsx
@@ -1,20 +1,41 @@
 import { InputForm } from '../../../components/common'
+import { ImCross } from 'react-icons/im'
 import { createProjectProp } from './types'
 
-export function LinkText({ text }: { text: string }): JSX.Element {
+
+// 'Токен не существует или срок его действия истек'
+// 'Токен позволяет получить доступ к более чем одному репозиторию на GitHub'
+function LinkText({ text }: { text: string }): JSX.Element {
     return <span className='font-semibold text-blue-600 underline hover:underline-offset-1 hover:cursor-pointer'>{text}</span>
+}
+
+function ErrorMessage({ text }: { text: string }): JSX.Element {
+    return (
+        <div className='w-full bg-rose-400/70 flex justify-start items-center mt-8 rounded-lg pt-2 pb-2 pr-2 pl-2'>
+            <p className='font-semibold pl-2'>{'🛑'}</p>
+            <p className='font-medium text-slate-100 pl-2'>{text}</p>
+        </div>
+    )
+}
+
+function WarningMessage({ text }: { text: string }): JSX.Element {
+    return (
+        <div className='w-full bg-orange-600/95 flex justify-start items-center mt-8 rounded-lg pt-2 pb-2 pr-2 pl-2'>
+            <p className='font-medium text-slate-100 pl-2'>{'⚠️'+text}</p>
+        </div>
+    )
 }
 
 export function ProjectsGithub({ projectCreate, setProjectCreate, setDisabled }: createProjectProp): JSX.Element {
     return (<>
         <div className='w-full bg-teal-200/60 flex flex-col justify-center items-start mb-8 rounded-lg pt-2 pb-2 pr-2 pl-2'>
-            <p className='pl-2'>Этот шаг необязателен.</p>
-            <p className='pl-2'>Вам потребуется <span className='font-bold'>github аккаунт</span> и <span className='font-bold'>токен</span> (<LinkText text='classic'/> или <LinkText text='fain-grain'/>).</p>
+            <p className='pl-2'>Этот шаг <span className='font-bold'>необязателен</span>.</p>
+            <p className='pl-2'>Вам потребуется <span className='font-bold'>github аккаунт</span> и <span className='font-bold'>токен</span> (<LinkText text='classic' /> или <LinkText text='fain-grain' />).</p>
         </div>
         <InputForm
             placeholder='GitHub token'
             type='password'
             classInput='border-none pl-5' />
-
+        <ErrorMessage text='Токен не существует или его срок действия истек' />
     </>)
 }
