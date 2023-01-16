bf1ddd9ff774eaa0e5c21f1c111e2ea3291a83ad
Sergey Kalutsky <skalutsky@gmail.com>
2023-01-17 00:07:09 +0600

    left only option for a fine-gtain-token

--- bridge/src/components/projects/create/ProjectsGithub.tsx
+++ bridge/src/components/projects/create/ProjectsGithub.tsx
@@ -12,7 +12,7 @@ export function ProjectsGithub(): JSX.Element {
     useEffect(() => {
         window.shared.incomingData("projects:checkgithubproject", async ({ type, msg, token }) => {
             if (type === 'error') {
-                setMessageJsx(<ErrorMessage text={msg} classDiv='mt-8 pt-2 pb-2 pr-2 pl-2'/>)
+                setMessageJsx(<ErrorMessage text={msg} classDiv='mt-8 pt-2 pb-2 pr-2 pl-2' />)
             } else {
                 if (!token.includes('github_pat')) {
                     const msg = 'Похоже что вы используете классический токен. Ради безопасности вашего GitHub аккаунта настоятельно рекомендуем перейти на Fine-Grain Token'
@@ -49,7 +49,7 @@ export function ProjectsGithub(): JSX.Element {
     return (<>
         <div className='w-full bg-teal-200/60 flex flex-col justify-center items-start mb-8 rounded-lg pt-2 pb-2 pr-2 pl-2'>
             <p className='pl-2'>Этот шаг <span className='font-bold'>необязателен</span>.</p>
-            <p className='pl-2'>Вам потребуется <span className='font-bold'>github аккаунт</span> и <span className='font-bold'>токен</span> (<LinkText text='classic' /> или <LinkText text='fain-grain' />).</p>
+            <p className='pl-2'>Вам потребуется <span className='font-bold'>github аккаунт</span> и <span className='font-bold'>токен</span> (<LinkText text='fain-grain-token' />).</p>
         </div>
         <div className='flex justify-center items-center '>
             <InputForm
