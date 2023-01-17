c1400a65dddc18a63748327a0d7ce5cf00d0ac34
Sergey Kalutsky <skalutsky@gmail.com>
2023-01-17 17:13:45 +0600

    refactored delete project

--- bridge/src/components/projects/popups/DeleteProjectPopUp.tsx
+++ bridge/src/components/projects/popups/DeleteProjectPopUp.tsx
@@ -1,33 +1,24 @@
 import PopUp from '../../common/PopuUp';
 import Button from '../../common/Button';
-import { UserProjects, Project } from '../types'
+import { Project } from '../types'
 import { useContext } from 'react';
 import { projectContext } from '../Projects';
 
-type Props = {
+
+export default function DeleteProjectPopUp({ projectDelete, open, setOpen }: {
     open: boolean
     projectDelete: Project
     setOpen: React.Dispatch<React.SetStateAction<boolean>>
-}
-
-export default function DeleteProjectPopUp({ projectDelete, open, setOpen }: Props): JSX.Element {
-    const { userProjects, setUserProjects } = useContext(projectContext);
+}): JSX.Element {
+    const { deleteProject } = useContext(projectContext);
     const handleClick = () => {
-        if (projectDelete.name === userProjects.activeProject.name) {
-            window.settings.del('active_project');
-        }
-        window.projects.delete(projectDelete.name);
-        setUserProjects({
-            ...userProjects,
-            projects: userProjects.projects.filter((project) => { return project.name != projectDelete.name; })
-        });
-
+        deleteProject(projectDelete)
         setOpen(false);
     };
     return (
         <PopUp
             open={open}
-            onClose={() => { setOpen(false); } }>
+            onClose={() => { setOpen(false); }}>
             <div>Вы уверены, что хотите удалить проект?</div>
             <Button onClick={handleClick} btnText='Удалить' />
         </PopUp>
