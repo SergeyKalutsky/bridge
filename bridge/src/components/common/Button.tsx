1f29146b3174dfefa7d5b485fd97d3ee388555c3
Sergey Kalutsky <skalutsky@gmail.com>
2023-01-21 00:43:09 +0600

    updated styles for button

--- bridge/src/components/common/Button.tsx
+++ bridge/src/components/common/Button.tsx
@@ -1,19 +1,19 @@
 const themes = {
     default: {
         base: 'rounded-md text-gray-200 bg-sky-800 font-medium',
-        hover: 'hover:bg-sky-400'
+        hover: 'hover:bg-sky-500/70'
     },
     purple: {
         base: 'rounded-md text-white bg-violet-500 font-medium',
         hover: 'hover:bg-violet-300'
     },
     teal: {
-        base: 'rounded-md text-white bg-teal-500 font-medium',
-        hover: 'hover:bg-teal-200'
+        base: 'rounded-md text-white bg-green-600 font-medium',
+        hover: 'hover:bg-green-500'
     },
     danger: {
         base: 'rounded-md text-white bg-red-600/80 font-medium',
-        hover: 'hover:bg-red-800'
+        hover: 'hover:bg-red-500'
     }
 }
 
