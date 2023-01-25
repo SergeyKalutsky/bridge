32558beab043d0f580443f723087e6d4b304bee2
Sergey Kalutsky <skalutsky@gmail.com>
2023-01-25 16:59:58 +0600

    changed icon placement

--- bridge/src/components/Editor/Editor.tsx
+++ bridge/src/components/Editor/Editor.tsx
@@ -57,22 +57,22 @@ const Editor = (): JSX.Element => {
             <Workspace>
                 <ToolBar>
                     <div className="w-full flex justify-end">
-                        <div className="flex justify-between w-[150px] mr-10 h-1/5">
+                        <div className="flex justify-between w-[160px] mr-10 h-1/5">
                             <IconButton onClick={handlePlayButtonClick}>
-                                <IoMdPlay style={{ color: '#76de85', height: 30, width: 30 }} />
+                                <IoMdPlay style={{ color: '#76de85', height: 30, width: 35 }} />
                             </IconButton>
-                            <IconButton onClick={handeCanselBattonClick}>
+                            {/* <IconButton onClick={handeCanselBattonClick}>
                                 <FaStop style={{ color: '#d91a1a', height: 25, width: 25 }} />
-                            </IconButton>
-                            {/* <IconButton onClick={() => { window.git.commit() }}>
-                                <IoMdGitCommit style={{ color: 'white', height: 50, width: 50 }} />
                             </IconButton> */}
                             <div className="flex">
-                                <IconButton >
-                                    <FaLongArrowAltDown style={{ color: 'white', height: 35, width: 30, textDecorationColor: 'white' }} />
+                                <IconButton onClick={() => { window.git.commit() }}>
+                                    <IoMdGitCommit style={{ color: 'white', height: 45, width: 45 }} />
                                 </IconButton>
                                 <IconButton >
-                                    <FaLongArrowAltUp style={{ color: 'white', height: 35, width: 30, textDecorationColor: 'white' }} />
+                                    <FaLongArrowAltDown style={{ color: 'white', height: 30, width: 30, textDecorationColor: 'white' }} />
+                                </IconButton>
+                                <IconButton >
+                                    <FaLongArrowAltUp style={{ color: 'white', height: 30, width: 30, textDecorationColor: 'white' }} />
                                 </IconButton>
                             </div>
                         </div>
