35ec5775776873e3ff2796e5130a655392386a96
Sergey Kalutsky <skalutsky@gmail.com>
2023-01-26 15:55:42 +0600

    refactored PopUp component

--- bridge/src/components/common/PopuUp.tsx
+++ bridge/src/components/common/PopuUp.tsx
@@ -1,27 +1,30 @@
 import Popup from 'reactjs-popup';
+import { PopupPosition } from 'reactjs-popup/dist/types';
 
-interface Props {
+
+function PopUp({ open, children, onClose, width = 40, height = 20, opacity=95, position='top center' }: {
     children?: React.ReactNode
     open: boolean
     onClose: () => void
     key?: string | number
-}
-
-const PopUp = ({ open, children, onClose }: Props): JSX.Element => {
+    width?: number
+    height?: number
+    opacity?: number
+    position?: PopupPosition
+}): JSX.Element {
     return (
         <Popup
-            contentStyle={{backgroundColor: 'transparent',
-            width: '40%'}}
+            contentStyle={{ width: `${width}%`, height: `${height}%` }}
             open={open}
             onClose={onClose}
             closeOnDocumentClick
-            position="right center"
+            position={position}
             modal>
-            <div className="p-3 gap-y-2 h-fit flex flex-col justify-center items-center bg-slate-300 rounded-md opacity-95 text-slate-900 text-2xl text-bold">
+            <div className={`p-3 gap-y-2 h-fit flex flex-col justify-center items-center bg-slate-400 rounded-md opacity-${opacity} text-slate-900 text-2xl text-bold`}>
                 {children}
             </div>
         </Popup>
-    )
+    );
 }
 
 export default PopUp
